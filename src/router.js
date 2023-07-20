158c158bdf784f3a38c78024fdb0d7d9c86318f9
ekhaart-dev <ekhaart02@gmail.com>
2023-07-20 17:00:57 +0700

    update landing_page

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./page/landing_page";
+import SplashScreen from "../src/component/landing_page/splashscreen";
+import PersonalProfile from "./page/personalProfile";
+import Home from "./page/home";
+
+function Router() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {isLoading ? (
+          <Route path="/" element={<SplashScreen />} />
+        ) : (
+          <>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/Home" element={<Home/>}/>
+            <Route path="/personal-profile" element={<PersonalProfile />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
